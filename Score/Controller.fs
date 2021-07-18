b0e5e121af833a47bda4254087b1189647f534a4
Vilson Fabricio Juliatto <fabricio@juliatto.com>
2021-07-18 17:48:58 -0300

    feat: improve error handling on Score.Controller
    Now we are handling possible exceptions thrown by the JSON deserializer

--- Score/Controller.fs
+++ Score/Controller.fs
@@ -2,24 +2,37 @@ module Score.Controller
 
 open Config
 open FSharp.Control.Tasks
+open Giraffe
 open Microsoft.AspNetCore.Http
 open Saturn.ControllerHelpers
 open Score.Commands
+open System.Text.Json
 
 let getPersonalId ctx id =
     Controller.text ctx (sprintf "%d" id)
 
+let handleJsonException (ctx: HttpContext) =
+    RequestErrors.badRequest (json {| error = "Bad Input!" |}) earlyReturn ctx
+
+let handleOtherExceptions (ctx: HttpContext) =
+    ServerErrors.internalError (json {| error = "Internal server error" |}) earlyReturn ctx
+
 let submitPersonalId (ctx: HttpContext) =
     task {
-        let config: Config = Controller.getConfig ctx
-        let connectionString = config.ConnectionString
+        try
+            let config: Config = Controller.getConfig ctx
+            let connectionString = config.ConnectionString
 
-        let! scoreDto = Controller.getJson<CreateScoreDto> ctx
+            let! scoreDto = Controller.getJson<CreateScoreDto> ctx
 
-        let! result = createScore connectionString scoreDto
-        match result with
-        | Ok(_) -> return! Controller.text ctx "ok"
-        | Error(_) -> return! Controller.text ctx "error"
+            let! result = createScore connectionString scoreDto
+            match result with
+            | Ok(_) -> return! Controller.text ctx "ok"
+            | Error(_) -> return! Controller.text ctx "error"
+
+        with
+        | :? JsonException -> return! handleJsonException ctx
+        | _ -> return! handleOtherExceptions ctx
     }
 
 let controller = Saturn.Controller.controller {
