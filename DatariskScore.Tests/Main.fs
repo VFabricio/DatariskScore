428f81919d14f82e18183cf70b4f9220b5262189
Vilson Fabricio Juliatto <fabricio@juliatto.com>
2021-07-24 20:50:34 -0300

    feat: create test project

---
+++ DatariskScore.Tests/Main.fs
@@ -0,0 +1,33 @@
+﻿module DatariskScore.Tests
+
+open System
+open Expecto
+open FSharp.Control.Tasks
+open Microsoft.AspNetCore.Hosting
+open Microsoft.Extensions.Hosting
+open Microsoft.AspNetCore.TestHost
+open System.Net
+
+let configureWebHostBuilder (builder: IWebHostBuilder) =
+    builder.UseTestServer() |> ignore
+
+let createHost () =
+    Program.app.ConfigureWebHost(Action<IWebHostBuilder> configureWebHostBuilder)
+
+let getScore = task {
+    use host = createHost().Start()
+    let client = host.GetTestClient()
+    let! response = client.GetAsync("/score/12345678901")
+    let code = response.StatusCode
+    Expect.equal code HttpStatusCode.NotFound "the response must be a 404"
+}
+
+[<Tests>]
+let tests =
+    testList "HTTP integration tests" [
+        testCaseAsync "foo" (Async.AwaitTask getScore)
+    ]
+
+[<EntryPoint>]
+let main argv =
+    Tests.runTestsInAssembly defaultConfig argv
