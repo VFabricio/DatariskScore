9ca1c83f2447f48f9108f78c299af45f3acf4a22
Vilson Fabricio Juliatto <fabricio@juliatto.com>
2021-07-24 21:10:37 -0300

    test: add postCpfNoCpf test

--- DatariskScore.Tests/Main.fs
+++ DatariskScore.Tests/Main.fs
@@ -8,12 +8,14 @@ open Microsoft.Extensions.Hosting
 open Microsoft.AspNetCore.TestHost
 open System.Net
 open System.Net.Http
+open System.Net.Http.Json
 
 let configureWebHostBuilder (builder: IWebHostBuilder) =
     builder.UseTestServer() |> ignore
 
 let createHost () =
-    Program.app.ConfigureWebHost(Action<IWebHostBuilder> configureWebHostBuilder)
+    let app = Program.createApp()
+    app.ConfigureWebHost(Action<IWebHostBuilder> configureWebHostBuilder)
 
 let postCpfNotJson = task {
     use host = createHost().Start()
@@ -28,10 +30,24 @@ let postCpfNotJson = task {
         "a POST with a non-JSON body should return 400"
 }
 
+let postCpfNoCpf = task {
+    use host = createHost().Start()
+    let client = host.GetTestClient()
+
+    let body = JsonContent.Create({| |})
+    let! response = client.PostAsync("/score", body)
+
+    Expect.equal
+        response.StatusCode
+        HttpStatusCode.BadRequest
+        "a POST without the CPF property should return 400"
+}
+
 [<Tests>]
 let tests =
     testList "HTTP integration tests" [
         testCaseAsync "postCpfNotJson" (Async.AwaitTask postCpfNotJson)
+        testCaseAsync "postCpfNoCpf" (Async.AwaitTask postCpfNoCpf)
     ]
 
 [<EntryPoint>]
